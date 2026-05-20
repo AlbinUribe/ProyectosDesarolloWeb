a247dc56a4f9d028a0e058becbd34c2134b00ea1
Albin Uribe <AlbinUribe@outlook.com>
2026-05-19 20:04:49 -0400

    Ejercicios Pagina 47

---
+++ Ejericicio2/src/main/java/com/mycompany/ejericicio2/Ejericicio2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejericicio2;
+
+/**
+ *
+ * @author Zarter
+ */
+public class Ejericicio2 {
+
+    public static void main(String[] args) {
+       
+        String Cadena = "200";
+        
+        int E = Integer.parseInt(Cadena);
+        
+        System.out.println(E);
+        
+    }
+}
