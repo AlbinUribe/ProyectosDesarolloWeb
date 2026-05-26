899ea8dd50db2f98ab86060dc5a462ce43afe870
Zarterken <albinuribe1@gmail.com>
2026-05-26 19:32:27 -0400

    Add files via upload

---
+++ Ejercicio4/src/main/java/com/mycompany/ejercicio4/Ejercicio4.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio4;
+
+/**
+ *
+ * @author Zarter
+ */
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        
+        String Cadena = "200";
+        
+        float f1 = Float.parseFloat(Cadena);
+        
+        System.out.println(f1);
+        
+    }
+}
